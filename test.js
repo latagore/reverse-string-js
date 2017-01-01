b93823261faea29bc8ae64b19430b26c1e715bb8
Steven Xu <stevenxu@live.ca>
2017-01-01 15:13:39 -0500

    add random mocha tests

--- test.js
+++ test.js
@@ -3,7 +3,19 @@ let reverse = require("./reverse.js")
 let should = require('chai').should();
 
 describe('reverse()', function() {
-  it('should return olleh when using hello', function() {
+  it('should reverse "hello"', function() {
     reverse("hello").should.equal("olleh");
+  });
+  
+  it('should reverse "You are a banana!"', function() {
+    reverse("You are a banana!").should.equal("!ananab a era uoY");
+  });
+  
+  it('should reverse "Marge let a moody baby doom a telegram."', function() {
+    reverse("Marge let a moody baby doom a telegram.").should.equal(".margelet a mood ybab ydoom a tel egraM");
+  });
+  
+  it('should reverse the reverse of "Hello World!"', function () {
+    reverse(reverse("Hello world!")).should.equal("Hello world!");
   });
 });
