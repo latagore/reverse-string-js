0ccc93290cdc02b1644c4a21dd76481d66b22b33
Steven Xu <stevenxu@live.ca>
2017-01-01 14:57:26 -0500

    explain we're using underscore's reverse to learn node

--- reverse.js
+++ reverse.js
@@ -3,5 +3,6 @@ let reverse = require("underscore.string/reverse");
 
 // returns a string where the characters are in reversed order
 module.exports = function(string) {
+  // could write the code ourselves... but better to learn how to use require instead. the algorithm is trivial anyways.
   return reverse(string);
 }
